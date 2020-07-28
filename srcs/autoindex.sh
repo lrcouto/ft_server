c1bf266b4603891ae7fc9add45545ab46b5c5955
L. R. Couto <57910428+lrcouto@users.noreply.github.com>
2020-07-28 18:46:50 -0300

    Adding autoindex.sh

---
+++ srcs/autoindex.sh
@@ -0,0 +1,24 @@
+# NGINX config file location.
+
+nginx_file='/etc/nginx/sites-enabled/default'
+
+# Searches file for "autoindex on".
+grep "autoindex on;" "$nginx_file" > /dev/null
+
+# If it finds "autoindex on", switches it OFF.
+if [ "$?" = "0" ]; then
+	echo "AutoIndex is currently ON. Turning it OFF"
+	sed -i 's/autoindex on/autoindex off/g' "$nginx_file"
+	autoindex="OFF";
+
+# Else, switches it ON.
+else
+	echo "AutoIndex is currently OFF. Turning it ON"
+	sed -i "s/autoindex off/autoindex on/g" "$nginx_file"
+	autoindex="ON";
+fi
+
+# Restarts NGINX and confirms switch.
+echo "Restarting NGINX"
+/etc/init.d/nginx restart
+echo -e "AutoIndex is now $autoindex!"
