3663ad478d0a31ffcf0821e511b12d4119d2b692
L. R. Couto <57910428+lrcouto@users.noreply.github.com>
2020-07-28 20:01:58 -0300

    Fixing syntax error.

--- srcs/autoindex.sh
+++ srcs/autoindex.sh
@@ -7,15 +7,15 @@ grep "autoindex on;" "$nginx_file" > /dev/null
 # If it finds "autoindex on", switches it OFF.
 if [ "$?" = "0" ]; then
 	echo "AutoIndex is currently ON. Turning it OFF"
-	sed -i 's/autoindex on/autoindex off/g' "$nginx_file"
-	sed -i 's/root /var/www/localhost;/root /var/www/localhost/wordpress;/g' "$nginx_file"
+	sed -i 's|autoindex on|autoindex off|g' "$nginx_file"
+	sed -i 's|root /var/www/localhost;|root /var/www/localhost/wordpress;|g' "$nginx_file"
 	autoindex="OFF";
 
 # Else, switches it ON.
 else
 	echo "AutoIndex is currently OFF. Turning it ON"
 	sed -i 's/autoindex off/autoindex on/g' "$nginx_file"
-	sed -i 's/root /var/www/localhost/wordpress;/root /var/www/localhost;/g' "$nginx_file"
+	sed -i 's|root /var/www/localhost/wordpress;|root /var/www/localhost;|g' "$nginx_file"
 	autoindex="ON";
 fi
 
